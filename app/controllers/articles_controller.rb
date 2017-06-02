e10bbabead191d16953285a8dc9f4c217e7bcd24
Jasdip Chauhan <jasdip.chauhan@shopify.com>
2017-06-02 14:32:45 -0400

    working on c, r, u methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
   def create
-
     @article = Article.new(article_params)
 
     if @article.save
@@ -14,12 +17,25 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-
   end
-
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:update] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
   end
 
 
